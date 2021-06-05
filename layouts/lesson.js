5794059a94163f12ceecef6ba97c377c10d4ebe7
Scott Moss <willscottmoss@gmail.com>
2021-06-05 14:06:16 -0700

    styles

--- layouts/lesson.js
+++ layouts/lesson.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import Page from './page'
 
 const LessonLayout = ({ lessons, content, frontMatter }) => {
+  const router = useRouter()
   const index = lessons.findIndex(l => l.title === frontMatter.title)
   const prev = lessons[index - 1]
   const next = lessons[index + 1]
@@ -14,18 +16,22 @@ const LessonLayout = ({ lessons, content, frontMatter }) => {
           className="absolute top-0 left-0 b-0 border-r border-gray-100 border-solid w-56 dark:border-gray-900"
           style={{ height: `calc(100vh - ${60}px)`, marginTop: '60px' }}
         >
-          <div className="p-6">
-            {lessons.map((l, i) => (
-              <div key={l.slug} className="my-2">
-                <Link href={`/lesson/${l.slug}`}>
-                  <a>
-                    <div>
-                      <p>{`${i + 1}. ${l.title}`}</p>
-                    </div>
-                  </a>
-                </Link>
-              </div>
-            ))}
+          <div>
+            {lessons.map((l, i) => {
+              const href = `/lesson/${l.slug}`
+              const active = href === router.asPath
+              return (
+                <div key={l.slug} className={`p-3 ${active ? 'bg-green-500' : ''}`}>
+                  <Link href={href}>
+                    <a>
+                      <div>
+                        <p className="font-thin">{l.title}</p>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              )
+            })}
           </div>
         </div>
         <div className="ml-56 p-6" style={{ height: `calc(100vh - ${60}px)`, overflowY: 'auto' }}>
@@ -42,8 +48,11 @@ const LessonLayout = ({ lessons, content, frontMatter }) => {
               {prev ? (
                 <Link href={`/lesson/${prev.slug}`}>
                   <a>
-                    <button type="button" className="h-12 text-base ring-2 px-4 py-2 rounded bg-green-500">
-                      Previous
+                    <button
+                      type="button"
+                      className="h-12 text-base ring-4 ring-green-200 px-4 py-2 rounded bg-green-500 text-white"
+                    >
+                      PREVIOUS
                     </button>
                   </a>
                 </Link>
@@ -53,8 +62,11 @@ const LessonLayout = ({ lessons, content, frontMatter }) => {
               {next ? (
                 <Link href={`/lesson/${next.slug}`}>
                   <a>
-                    <button type="button" className="h-12 text-base ring-2 px-4 py-2 rounded bg-green-500">
-                      Next
+                    <button
+                      type="button"
+                      className="h-12 text-base ring-4 ring-green-200 px-4 py-2 rounded bg-green-500 text-white"
+                    >
+                      NEXT
                     </button>
                   </a>
                 </Link>
